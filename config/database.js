da708b6321f23fca0f02036df3fb60ea394453f6
Mirado111 <miradofrancky@gmail.com>
2019-05-21 00:18:28 +0300

    src app

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: "mongodb://diary:diary@ds151908.mlab.com:51908/youplay",
+  database :"mongodb://rlmf:Immortel@youtubecluster0-shard-00-00-qitf6.mongodb.net:27017,youtubecluster0-shard-00-01-qitf6.mongodb.net:27017,youtubecluster0-shard-00-02-qitf6.mongodb.net:27017/test?ssl=true&replicaSet=YoutubeCluster0-shard-0&authSource=admin&retryWrites=true",
+  secret:"MiradoRandria"
+}
